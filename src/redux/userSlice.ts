a1da9bdd4073bd350cde210e3fe4314ff10c4f20
Dmitriy <dmitrizhivoderov@mail.ru>
2024-12-04 16:58:28 +0400

    реализовал слайс для информации пользователя (добавил действия регистрации, входа и выхода)

--- src/redux/userSlice.ts
+++ src/redux/userSlice.ts
@@ -1,4 +1,6 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { log } from "console";
+import { stat } from "fs";
 
 interface UserState {
     isAuthenticated: boolean;
@@ -16,7 +18,18 @@ const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
-
+        register: (state, action: PayloadAction<{ name: string; email: string }>) => {
+            state.isAuthenticated = true;
+            state.userInfo = action.payload;
+        },
+        login: (state, action: PayloadAction<{ email: string }>) => {
+            state.isAuthenticated = true;
+            state.loginSuccess = true;
+        },
+        logout: (state) => {
+            state.isAuthenticated = false;
+            state.loginSuccess = false;
+        },
     },
 });
 
