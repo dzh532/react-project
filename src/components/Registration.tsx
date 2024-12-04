e5ac2a292f1ec917fc41b1f71184c07836965e94
Dmitriy <dmitrizhivoderov@mail.ru>
2024-12-04 17:28:34 +0400

    добавил разметку на страницу регистрации

--- src/components/Registration.tsx
+++ src/components/Registration.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Registration: React.FC = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = () => {
+
+    };
+
     return (
         <div>
-            
+            <h1>Регистрация</h1>
+
+            <form onSubmit={handleSubmit}>
+                <p>Имя</p>
+                <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+
+                <p>Почта</p>
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+
+                <p>Пароль</p>
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+
+                <button type="submit">Зарегистрироваться</button>
+            </form>
         </div>
     );
 };
