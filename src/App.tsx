ca7313df7b64a9dfece90183aabbad26331d8412
Dmitriy <dmitrizhivoderov@mail.ru>
2024-12-04 16:25:48 +0400

    подключил react-router-dom и создал страницу приветствия

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
+import store from './redux/store';
+
+import Welcome from './components/Welcome';
 
 const App = () => (
-
+  <Provider store={store}>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+      </Routes>
+    </Router>
+  </Provider>
 );
 
 export default App;
